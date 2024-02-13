a6474111c4899d52a4440ba0aadb10899daf4126
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-02-13 10:33:01 +0100

    [NEW] Logica de seleccion de heroe y villano compeltada

--- Index.js
+++ Index.js
@@ -1,3 +1,16 @@
 import { StartNormalGame }  from "./controllers/GameController.js";
 
 StartNormalGame();
+
+
+// import { getVillain,getSuperHero,getDado100C,getDado20C,getDado3C} from "./functions.mjs";
+
+// const villainZarate = getVillain(apiData);
+
+// const superHero = getSuperHero(apiData);
+
+
+// const D100C = getDado100C();
+// const D20C = getDado20C();
+// const D3C = getDado3C();
+
--- controllers/GameController.js
+++ controllers/GameController.js
@@ -2,16 +2,46 @@
 
 import GameService from "../services/GameService.js";
 
+
+// GAME START
 const StartNormalGame = async () => {
   try {
     const allSuperPeople = await GameService.getData();
     if (allSuperPeople.length === 0) {
       return { message: 'No existen superPersonas' };
     }
-    console.log(allSuperPeople);
+
+	const villain = getVillain(allSuperPeople);
+	const superHero = getHero(allSuperPeople)
+	
   } catch (error) {
     console.error('Error al obtener datos:', error);
   }
 };
+
+// ============================
+// 			FUNCTIONS
+// ============================
+
+//Funcion que selecciona al villano de todos los heroes
+const getVillain =(allSuperPeople) => {
+	return allSuperPeople.filter((superPerson) => superPerson.name === "Junkpile");
+}
+
+// Funcion que elige un heroe aleatorio (No puede elegir a Junkpile)
+const getHero = (allSuperPeople) => {
+
+	const hero = allSuperPeople[randomSuperHeroesNumber(allSuperPeople)];
+
+	if (hero.name == "Junkpile")
+	{ hero = allSuperPeople[randomSuperHeroesNumber(allSuperPeople)] }
+
+	return hero;
+}
+
+const randomSuperHeroesNumber = (allSuperPeople) => { return Math.floor(Math.random() * allSuperPeople.length) }
+
+
+
 
 export { StartNormalGame };
